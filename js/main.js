2c9fc007869fbad5ea2bbddb7a3297738453cc0b
Won Hyoung Lee <circlelee92@gmail.com>
2025-06-15 13:09:15 +0900

    add skill set and refine display

--- js/main.js
+++ js/main.js
@@ -1,33 +1,43 @@
-
 var $g_jsonData = null;
 
 function loadData() {
-    $.getJSON('./assets/data.json', function (data) {
+    $.getJSON("./assets/data.json", function (data) {
         $g_jsonData = data;
     });
 }
-
 
 function addProEx(idx, jsonData) {
     var title = jsonData.title;
     var company_name = jsonData.company_name;
     var period = jsonData.period;
     var role = jsonData.role;
+    var skillSets = jsonData.skillSets;
     var projects = jsonData.projects;
     var state = jsonData.state;
     var loc = jsonData.loc;
 
-    var $div_content = $("<div class='content "+ state +"'></div>");
-    $div_content.append('<p>' + title + ", " + period + '</p>');
-    $div_content.append('<p class="white-space"><i class="fas fa-building"></i> ' + company_name + '</p>');
-    
+    var $div_content = $("<div class='content " + state + "'></div>");
+    $div_content.append("<p>" + title + ", " + period + "</p>");
+    $div_content.append('<p class="white-space"><i class="fas fa-building"></i> ' + company_name + "</p>");
+
     if (!!role) {
-        $div_content.append('<p class="white-space"><i class="fas fa-keyboard"></i> ' + role + '</p>');
+        $div_content.append('<p class="white-space"><i class="fas fa-keyboard"></i> ' + role + "</p>");
     }
 
-    $.each(projects, function (idx, project) {
-        $div_content.append('<p class="deeper-white-space"><i class="fas fa-check"></i> ' + project + '</p>');
-    });
+    if (!!skillSets) {
+        $div_content.append('<p class="white-space"><i class="fas fa-code"></i> Skills</p>');
+        $.each(skillSets, function (idx, skillSet) {
+            var skills = skillSet.join(", ");
+            $div_content.append('<p class="deeper-white-space"><i class="fas fa-caret-right"></i> ' + skills + "</p>");
+        });
+    }
+
+    if (!!projects) {
+        $div_content.append('<p class="white-space"><i class="fas fa-clipboard-list"></i> Projects</p>');
+        $.each(projects, function (idx, project) {
+            $div_content.append('<p class="deeper-white-space"><i class="fas fa-caret-right"></i> ' + project + "</p>");
+        });
+    }
 
     var $div_line_l = $("<div class='line-l'></div>");
     var $div_line_r = $("<div class='line-r'></div>");
@@ -38,14 +48,13 @@ function addProEx(idx, jsonData) {
 
         $div_line_l.append($div_content);
         $div_line_l.append('<div class="link-line"></div>');
-        $div_line_r.addClass('line-e');
-    }
-    else {
+        $div_line_r.addClass("line-e");
+    } else {
         $div_line_loc = $div_line_r;
 
         $div_line_r.append('<div class="link-line"></div>');
         $div_line_r.append($div_content);
-        $div_line_l.addClass('line-e');
+        $div_line_l.addClass("line-e");
     }
 
     $div_line_loc.append();
@@ -54,7 +63,7 @@ function addProEx(idx, jsonData) {
     $div_line.append($div_line_l);
     $div_line.append($div_line_r);
 
-    $('#pro-ex').append($div_line);
+    $("#pro-ex").append($div_line);
 }
 
 function addAcademicEx(idx, jsonData) {
@@ -67,7 +76,7 @@ function addAcademicEx(idx, jsonData) {
     var loc = jsonData.loc;
 
     var $div_content = $("<div class='content'></div>");
-    $div_content.append('<p>' + title + ", " + period + '</p>');
+    $div_content.append("<p>" + title + ", " + period + "</p>");
 
     if (!!name) {
         $div_content.append("<p class='white-space'><i class='fas fa-university'></i> " + name + "</p>");
@@ -82,7 +91,7 @@ function addAcademicEx(idx, jsonData) {
     }
 
     $.each(details, function (idx, detail) {
-        $div_content.append('<p class="deeper-white-space"><i>- ' + detail + '</i></p>');
+        $div_content.append('<p class="deeper-white-space"><i>- ' + detail + "</i></p>");
     });
 
     var $div_line_l = $("<div class='line-l'></div>");
@@ -94,14 +103,13 @@ function addAcademicEx(idx, jsonData) {
 
         $div_line_l.append($div_content);
         $div_line_l.append('<div class="link-line"></div>');
-        $div_line_r.addClass('line-e');
-    }
-    else {
+        $div_line_r.addClass("line-e");
+    } else {
         $div_line_loc = $div_line_r;
 
         $div_line_r.append('<div class="link-line"></div>');
         $div_line_r.append($div_content);
-        $div_line_l.addClass('line-e');
+        $div_line_l.addClass("line-e");
     }
 
     $div_line_loc.append();
@@ -110,7 +118,7 @@ function addAcademicEx(idx, jsonData) {
     $div_line.append($div_line_l);
     $div_line.append($div_line_r);
 
-    $('#academic-ex').append($div_line);
+    $("#academic-ex").append($div_line);
 }
 
 function addEdu(idx, jsonData) {
@@ -122,13 +130,13 @@ function addEdu(idx, jsonData) {
     var detail = jsonData.detail;
 
     var $div_content = $("<div class='content'></div>");
-    $div_content.append('<p>' + degree_name + ", " + period + '</p>');
-    $div_content.append('<p class="white-space"><i class="fas fa-university"></i> ' + school + '</p>');
+    $div_content.append("<p>" + degree_name + ", " + period + "</p>");
+    $div_content.append('<p class="white-space"><i class="fas fa-university"></i> ' + school + "</p>");
     if (!!lab) {
-        $div_content.append('<p class="white-space"><i class="fas fa-desktop"></i> ' + lab + '</p>');
+        $div_content.append('<p class="white-space"><i class="fas fa-desktop"></i> ' + lab + "</p>");
     }
     if (!!detail) {
-        $div_content.append('<p class="edu-detail">' + detail + '</p>');
+        $div_content.append('<p class="edu-detail">' + detail + "</p>");
     }
 
     var $div_line_l = $("<div class='line-l'></div>");
@@ -140,14 +148,13 @@ function addEdu(idx, jsonData) {
 
         $div_line_l.append($div_content);
         $div_line_l.append('<div class="link-line"></div>');
-        $div_line_r.addClass('line-e');
-    }
-    else {
+        $div_line_r.addClass("line-e");
+    } else {
         $div_line_loc = $div_line_r;
 
         $div_line_r.append('<div class="link-line"></div>');
         $div_line_r.append($div_content);
-        $div_line_l.addClass('line-e');
+        $div_line_l.addClass("line-e");
     }
 
     $div_line_loc.append();
@@ -156,21 +163,21 @@ function addEdu(idx, jsonData) {
     $div_line.append($div_line_l);
     $div_line.append($div_line_r);
 
-    $('#edu').append($div_line);
+    $("#edu").append($div_line);
 }
 
 function addOpenSrc(idx, jsonData) {
     var img_name = jsonData.img_name;
-    
+
     var $div_logo = $('<div class="open-src-logo"><img src="./assets/' + img_name + '"></div>');
-    $('#open-src-content').append($div_logo);
+    $("#open-src-content").append($div_logo);
 }
 
 function addTechStack(idx, jsonData) {
     var img_name = jsonData.img_name;
-    
+
     var $div_logo = $('<div class="tech-stack-logo"><img src="./assets/' + img_name + '"></div>');
-    $('#tech-stack-content').append($div_logo);
+    $("#tech-stack-content").append($div_logo);
 }
 
 function addPaper(id, jsonData) {
@@ -178,27 +185,26 @@ function addPaper(id, jsonData) {
     var title = jsonData.title;
     var journal = jsonData.journal;
     var link = jsonData.link;
-    
+
     var $div_paper = $('<div class="paper"></div>');
-    $div_paper.append($('<p>' + author + '</p>'));
+    $div_paper.append($("<p>" + author + "</p>"));
     if (link !== undefined && link !== "") {
         $div_paper.append($('<p class="title"><a target="_blank" href="' + link + '">"' + title + '"</a></p>'));
-    }
-    else {
+    } else {
         $div_paper.append($('<p class="title">"' + title + '"</p>'));
     }
-    $div_paper.append($('<p class="journal">' + journal + '</p>'));
+    $div_paper.append($('<p class="journal">' + journal + "</p>"));
 
     $(id).append($div_paper);
 }
 function addInternationalJournal(idx, jsonData) {
-    addPaper('#inter_jnl', jsonData);
+    addPaper("#inter_jnl", jsonData);
 }
 function addDomesticJournal(idx, jsonData) {
-    addPaper('#dome_jnl', jsonData);
+    addPaper("#dome_jnl", jsonData);
 }
 function addConference(idx, jsonData) {
-    addPaper('#conf', jsonData);
+    addPaper("#conf", jsonData);
 }
 
 function addPatent(id, jsonData) {
@@ -206,31 +212,29 @@ function addPatent(id, jsonData) {
     var title = jsonData.title;
     var date = jsonData.date;
     var link = jsonData.link;
-    
+
     var $div_patent = $('<div class="patent"></div>');
-    $div_patent.append($('<p>' + author + '</p>'));
+    $div_patent.append($("<p>" + author + "</p>"));
     if (link !== undefined && link !== "") {
         $div_patent.append($('<p class="title"><a target="_blank" href="' + link + '">"' + title + '"</a></p>'));
-    }
-    else {
+    } else {
         $div_patent.append($('<p class="title">"' + title + '"</p>'));
     }
-    $div_patent.append($('<p>' + date + '</p>'));
+    $div_patent.append($("<p>" + date + "</p>"));
 
     $(id).append($div_patent);
 }
 function addInternationalPatent(idx, jsonData) {
-    addPatent('#inter_pat', jsonData);
+    addPatent("#inter_pat", jsonData);
 }
 function addDomesticPatent(idx, jsonData) {
-    addPatent('#dome_pat', jsonData);
+    addPatent("#dome_pat", jsonData);
 }
 
 function addAwards(id, jsonData) {
     var text = jsonData;
-    $('#awards').append($('<div class="award"><p>' + text + '</p></div>'))
+    $("#awards").append($('<div class="award"><p>' + text + "</p></div>"));
 }
-
 
 function renderData() {
     if ($g_jsonData == null) {
@@ -238,7 +242,7 @@ function renderData() {
         return;
     }
 
-    $('body').removeClass('hidden');
+    $("body").removeClass("hidden");
 
     $.each($g_jsonData.pro_ex, addProEx);
     $.each($g_jsonData.academic_ex, addAcademicEx);
